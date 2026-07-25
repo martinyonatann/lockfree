81ee83aace532ec124c79721d42ccc17dc034cf2
Martin Yonatan Pasaribu <107392127+martinyonatann@users.noreply.github.com>
2026-07-25 21:38:23 +0700

    feat(ring_buffer): support wrap-around indexing (#4)
    * feat(ring_buffer): implement push operation
    
    * feat(ring_buffer): support wrap-around indexing

--- tests/ring_buffer_test.cpp
+++ tests/ring_buffer_test.cpp
@@ -23,3 +23,77 @@ TEST(RingBuffer, PushOneElement)
     EXPECT_FALSE(buffer.full());
     EXPECT_EQ(buffer.size(), 1);
 }
+
+TEST(RingBuffer, PopOneElement)
+{
+    lockfree::RingBuffer<int, 8> buffer;
+    ASSERT_TRUE(buffer.push(42));
+
+    int value = 0;
+
+    ASSERT_TRUE(buffer.pop(value));
+
+    EXPECT_EQ(value, 42);
+    EXPECT_TRUE(buffer.empty());
+    EXPECT_EQ(buffer.size(), 0);
+}
+
+TEST(RingBuffer, PopFromEmptyBuffer)
+{
+    lockfree::RingBuffer<int, 8> buffer;
+    int value = 42;
+    EXPECT_FALSE(buffer.pop(value));
+    EXPECT_EQ(value, 42);
+}
+
+TEST(RingBuffer, PushIntoFullBuffer)
+{
+    lockfree::RingBuffer<int, 2> buffer;
+
+    EXPECT_TRUE(buffer.push(1));
+    EXPECT_TRUE(buffer.push(2));
+
+    EXPECT_TRUE(buffer.full());
+    EXPECT_EQ(buffer.size(), 2);
+    EXPECT_FALSE(buffer.push(3));
+    EXPECT_EQ(buffer.size(), 2);
+}
+
+TEST(RingBuffer, PreservesFifoOrder)
+{
+    lockfree::RingBuffer<int, 8> buffer;
+    ASSERT_TRUE(buffer.push(10));
+    ASSERT_TRUE(buffer.push(20));
+    ASSERT_TRUE(buffer.push(30));
+
+    int value = 0;
+    ASSERT_TRUE(buffer.pop(value));
+    EXPECT_EQ(value, 10);
+}
+
+TEST(RingBuffer, WrapAround)
+{
+    lockfree::RingBuffer<int, 3> buffer;
+
+    int value = 0;
+
+    ASSERT_TRUE(buffer.push(1));
+    ASSERT_TRUE(buffer.push(2));
+    ASSERT_TRUE(buffer.push(3));
+
+    ASSERT_TRUE(buffer.pop(value));
+    EXPECT_EQ(value, 1);
+
+    ASSERT_TRUE(buffer.push(4));
+
+    ASSERT_TRUE(buffer.pop(value));
+    EXPECT_EQ(value, 2);
+
+    ASSERT_TRUE(buffer.pop(value));
+    EXPECT_EQ(value, 3);
+
+    ASSERT_TRUE(buffer.pop(value));
+    EXPECT_EQ(value, 4);
+
+    EXPECT_TRUE(buffer.empty());
+}
