e0e85df116ebec5e04f2a0a8002eebc256ba8395
Martin Yonatan Pasaribu <107392127+martinyonatann@users.noreply.github.com>
2026-07-26 00:03:58 +0700

    feat(spsc_queue) functional SPSC Queue (#6)

--- tests/spsc_queue_test.cpp
+++ tests/spsc_queue_test.cpp
@@ -2,9 +2,11 @@
 
 #include <lockfree/spsc_queue.hpp>
 
+constexpr int DEFAULT_CAPACITY = 8;
+
 TEST(SPSCQueue, DefaultConstructedIsEmpty)
 {
-    lockfree::SPSCQueue<int, 8> queue;
+    lockfree::SPSCQueue<int, DEFAULT_CAPACITY> queue;
 
     EXPECT_TRUE(queue.empty());
     EXPECT_FALSE(queue.full());
@@ -13,14 +15,14 @@ TEST(SPSCQueue, DefaultConstructedIsEmpty)
 
 TEST(SPSCQueue, PushOneElement)
 {
-    lockfree::SPSCQueue<int, 8> queue;
+    lockfree::SPSCQueue<int, DEFAULT_CAPACITY> queue;
     EXPECT_TRUE(queue.push(42));
     EXPECT_FALSE(queue.empty());
 }
 
 TEST(SPSCQueue, PopOneElement)
 {
-    lockfree::SPSCQueue<int, 8> queue;
+    lockfree::SPSCQueue<int, DEFAULT_CAPACITY> queue;
     ASSERT_TRUE(queue.push(42));
 
     int value = 0;
@@ -28,5 +30,63 @@ TEST(SPSCQueue, PopOneElement)
     ASSERT_TRUE(queue.pop(value));
 
     EXPECT_EQ(value, 42);
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(SPSCQueue, PushIntoFullQueue)
+{
+    lockfree::SPSCQueue<int, 2> queue;
+
+    EXPECT_TRUE(queue.push(1));
+    EXPECT_TRUE(queue.push(2));
+    EXPECT_TRUE(queue.full());
+    EXPECT_FALSE(queue.push(3));
+}
+
+TEST(SPSCQueue, PreservesFifoOrder)
+{
+    lockfree::SPSCQueue<int, DEFAULT_CAPACITY> queue;
+    ASSERT_TRUE(queue.push(10));
+    ASSERT_TRUE(queue.push(20));
+    ASSERT_TRUE(queue.push(30));
+
+    int value = 0;
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 10);
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 20);
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 30);
+
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(SPSCQueue, WrapAround)
+{
+    lockfree::SPSCQueue<int, 3> queue;
+
+    int value = 0;
+
+    ASSERT_TRUE(queue.push(1));
+    ASSERT_TRUE(queue.push(2));
+    ASSERT_TRUE(queue.push(3));
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 1);
+
+    ASSERT_TRUE(queue.push(4));
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 2);
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 3);
+
+    ASSERT_TRUE(queue.pop(value));
+    EXPECT_EQ(value, 4);
+
     EXPECT_TRUE(queue.empty());
 }
